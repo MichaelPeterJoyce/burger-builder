e61732934b88872a52aa02ae8b18369611c40638
Michael Joyce <michael.peter.joyce@gmail.com>
2018-04-17 08:19:44 +0100

    Added initial render of burger component. Added css styling for Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,1 +1,21 @@
- 
+import React, { PropTypes } from "react";
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+const burger = props => {
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type={"bread-top"}/>
+      <BurgerIngredient type={"Cheese"}/>
+      <BurgerIngredient type={"meat"}/>
+      <BurgerIngredient type={"Salad"}/>
+      <BurgerIngredient type={"bread-bottom"}/>
+    </div>
+  );
+};
+
+burger.propTypes = {};
+
+burger.defaultProps = {};
+
+export default burger;
