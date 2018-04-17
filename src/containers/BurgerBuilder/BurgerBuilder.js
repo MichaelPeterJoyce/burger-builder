038d7b8bd5fa6132fc9d21b66035556ce1f2bfeb
Michael Joyce <michael.peter.joyce@gmail.com>
2018-04-17 08:40:57 +0100

    Added first pass of generic dynamic rendering of burger and added message to popup if there are no ingredients.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,22 @@ import Aux from "../../hoc/Auxiliary";
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {}
+  // }
+  state = {
+    ingredients: {
+      Salad: 0,
+      Bacon: 0,
+      Cheese: 0,
+      meat: 0
+    }
+  };
   render() {
     return (
       <Aux>
-        <Burger/>
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Aux>
     );
