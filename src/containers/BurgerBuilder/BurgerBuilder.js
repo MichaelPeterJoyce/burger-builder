136df0a258410f1b7744a022e33d70c76f299711
Michael Joyce <michael.peter.joyce@gmail.com>
2018-04-23 19:51:16 +0100

    Started working with axios to send the orders to firebase to save the data. Implemented post method to save order to database

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.2,
@@ -77,7 +78,26 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Michael",
+        address: {
+          street: "123 Street",
+          zipCode: "12132",
+          country: "Ireland"
+        },
+        email: "michael.j@gmail.com"
+      },
+      deliveryMethod: "fastest",
+      date: new Date()
+    };
+
+    axios
+      .post("/orders.json", order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   };
 
   render() {
