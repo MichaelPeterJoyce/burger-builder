54deacceab4af64f8dc85549d8562fbd75031011
Michael Joyce <michael.peter.joyce@gmail.com>
2018-04-23 08:43:05 +0100

    Added nice burger icon to replace the word menu for the toolbar

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import classes from './DrawerToggle.css';
 
 const DrawerToggle = (props) => {
   return (
-    <div onClick={props.clicked}>MENU</div>
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   );
 };
 
