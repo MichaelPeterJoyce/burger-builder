43eef991c99cfbf6cc2d9d3558288503275ab205
Michael Joyce <michael.peter.joyce@gmail.com>
2018-04-24 08:49:36 +0100

    Added loading spinner when placing an order for a burger, added global error handling to open a popup which can be used to wrap other components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Auxillary/Auxiliary";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
+import { Dimmer, Loader, Image, Segment } from "semantic-ui-react";
 
 const INGREDIENT_PRICES = {
   salad: 0.2,
@@ -93,11 +95,15 @@ class BurgerBuilder extends Component {
       deliveryMethod: "fastest",
       date: new Date()
     };
-
+    this.setState({ loading: true });
     axios
       .post("/orders.json", order)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
+      .then(response => {
+        this.setState({ loading: false, purchasing: false });
+      })
+      .catch(error => {
+        this.setState({ loading: false, purchasing: false });
+      });
   };
 
   render() {
@@ -113,6 +119,11 @@ class BurgerBuilder extends Component {
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}
         >
+          {/*Semantic UI Element for loading spinner when ajax request is being sent*/}
+          <Dimmer inverted active={this.state.loading}>
+            <Loader inverted>Loading</Loader>
+          </Dimmer>
+
           <OrderSummary
             cancel={this.purchaseCancelHandler}
             continue={this.purchaseContinueHandler}
@@ -133,5 +144,4 @@ class BurgerBuilder extends Component {
     );
   }
 }
-
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
